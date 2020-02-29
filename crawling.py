45bf843a4b7a91b92e1860ca875c571d71047727
Rahul Singh <rahul.singh4800@gmail.com>
2020-02-29 15:04:51 +0530

    blog scrapping from site

--- crawling.py
+++ crawling.py
@@ -1,15 +1,16 @@
 import json
 from time import sleep
 import requests
+import csv
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
+SAVED_FILE = 'blog_store/blog_dataset.csv'
 
-def crawler(max_pages):
+def crawler(url,max_pages):
     urls = []
     start = 1
-    while start <= max_pages:
-        #~ print(start)
-        url = 'https://www.analyticsvidhya.com/blog/category/machine-learning/page/' + str(start)
+    for i in range(0, max_pages):
+        url = url + str(start)
         source_code = requests.get(url, allow_redirects=True).content
         soup = BeautifulSoup(source_code, 'html.parser')
         # import pdb; pdb.set_trace()
@@ -17,18 +18,30 @@ def crawler(max_pages):
         for scrape in soup.find_all("h3", class_="entry-title"):
             for a in scrape.find_all('a'):
                 article = a.get('href')
-                urls.append(article)
-                print(article)
+                # need to transfer the data
+                status = content_crawler(article)
+                if status:
+                    print('{} <<==== url crawl completed'.format(a))
+                else:
+                    pass
         """End scrape"""
+    return True
 
-        start += 1
-        sleep(3)
+def content_crawler(url):
+    source_code = requests.get(url, allow_redirects=True).content
+    soup = BeautifulSoup(source_code, 'html.parser')
 
-    parsed = urlparse(url).netloc.split('.')[0]
-    filename = parsed
+    main_html = soup.find('div', {'class': 'text-content'})
+    share_removed = main_html.find('div', {'class':'sharedaddy sd-sharing-enabled'}).decompose()
+    related_posts = main_html.find('div', id="jp-relatedposts").decompose()
+    [s.decompose() for s in main_html('script')]
+    
+    open_file = open(SAVED_FILE, 'a')
+    writer = csv.writer(open_file)
+    writer.writerow([url, main_html])
 
-    with open(filename + '.json', 'a') as file_object:
-        json.dump(urls, file_object, indent=4)
-        print(filename, 'exported!')
+    return True
 
-crawler(41)
+if __name__ == '__main__':
+    contentUrl = 'https://www.analyticsvidhya.com/blog/category/deep-learning/page/'
+    crawler(contentUrl,41)
